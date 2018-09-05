151bc0e6f7eb1c7c43453a0e2b72dff00b8cb3bf
David Yearwood <Yearwood@ilstmd.ins>
2018-09-05 11:25:26 -0500

    Fix circular Queue head and tail pointers and create displayItems()

--- StackQueue/CircularQueue.js
+++ StackQueue/CircularQueue.js
@@ -1,7 +1,7 @@
 class CircularQueue {
     constructor(s) {
-        this._head = 0; 
-        this._tail = 0;
+        this._head = 0;
+        this._tail = -1;
         this._itemsCount = 0;
         this._maxSize = s;
         this._queue = [];
@@ -10,16 +10,18 @@ class CircularQueue {
     getSize() {
         return this._itemsCount;
     }
+
     insert(n) {
         if (this.isFull()) {
             throw new Error("Queue is full");
         }
 
         if (this._tail === this._maxSize - 1) {
-            this._tail = 0;
-        } 
+            this._tail = -1;
+        }
 
-        this._queue[this._tail++] = n; 
+        this._queue[++this._tail] = n;
+
         return ++this._itemsCount;
     }
 
@@ -31,26 +33,46 @@ class CircularQueue {
         var temp = this._queue[this._head];
 
         if (this._head === this._maxSize - 1) {
-            this._head = 0; 
+            this._head = 0;
         } else {
             ++this._head;
         }
 
         this._itemsCount--;
 
-        return temp; 
-     }
+        return temp;
+    }
 
     peek() {
-        return this._queue[this._head]; 
+        return this._queue[this._head];
     }
 
     isEmpty() {
-        return this._itemsCount === 0; 
+        return this._itemsCount === 0;
     }
 
     isFull() {
-        return this._itemsCount === this._maxSize; 
+        return this._itemsCount === this._maxSize;
+    }
+
+    // display
+    displayItems() {
+        var itemsToPrint = this._itemsCount,
+            currentPos = this._head,
+            output = "";
+        while (itemsToPrint > 0) {
+            output = (output === "") ?
+                this._queue[currentPos++] : output + ", " +
+                this._queue[currentPos++];
+
+            if (currentPos === this._maxSize) {
+                currentPos = 0;
+            }
+
+            itemsToPrint--;
+        }
+
+        return output;
     }
 }
 
--- test/CircularQueue.test.js
+++ test/CircularQueue.test.js
@@ -1,27 +1,27 @@
 const CircularQueue = require('../StackQueue/CircularQueue');
-const assert = require('chai').assert; 
+const assert = require('chai').assert;
+
+function createMockQueue() {
+    var queue = new CircularQueue(5);
+    queue.insert(10);
+    queue.insert(20);
+    queue.insert(30);
+    queue.insert(40);
+    return queue;
+}
 
 describe('CircularQueue', () => {
-    var queue; 
-
-    function createMockQueue() {
-        var queue = new CircularQueue(5); 
-        queue.insert(10);
-        queue.insert(20);
-        queue.insert(30);
-        queue.insert(40);
-        return queue;     
-    }
+    var queue;
 
     beforeEach(() => {
-        queue = createMockQueue(); 
+        queue = createMockQueue();
     });
 
     test('should return the correct size value', () => {
         assert.strictEqual(queue.getSize(), 4);
     });
 
-    test('wrap around when removing three items and adding four new items', () => {
+    test('should wrap around when removing three items and adding four new items', () => {
         queue.remove();
         queue.remove();
         queue.remove();
@@ -31,8 +31,66 @@ describe('CircularQueue', () => {
         queue.insert(70);
         queue.insert(80);
 
-        assert.strictEqual(queue._tail - 1, 3);
+        assert.strictEqual(queue._tail, 2);
     });
 
-    
+    test('head and tail should point to the correct index after operations', () => {
+
+        queue.remove();
+        queue.remove();
+        queue.remove();
+
+        queue.insert(50);
+        queue.insert(60);
+        queue.insert(70);
+        queue.insert(80);
+
+        assert.strictEqual(queue._tail, 2);
+
+    });
+
+    test('head should point to the correct index', () => {
+        queue.remove();
+        queue.remove();
+        queue.remove();
+
+        queue.insert(50);
+        queue.insert(60);
+        queue.insert(70);
+        queue.insert(80);
+
+        queue.remove();
+        queue.remove();
+
+        assert.strictEqual(queue._head, 0);
+    });
+
 });
+
+describe('Question 4.1 - displayItems()', () => {
+    var queue;
+
+    beforeEach(() => {
+        queue = createMockQueue();
+    });
+
+    test('should show the queue contents from the first item inserted to the list to the last', () => {
+        var expected = '10, 20, 30, 40';
+
+        assert.strictEqual(queue.displayItems(), expected);
+    });
+
+    test('should show the queue contents from the first item inserted to the list to the last', () => {
+        var expected = '40, 50, 60, 70, 80';
+        queue.remove();
+        queue.remove();
+        queue.remove();
+
+        queue.insert(50);
+        queue.insert(60);
+        queue.insert(70);
+        queue.insert(80);
+
+        assert.strictEqual(queue.displayItems(), expected);
+    });
+})
