94364cf7f96ea9120b438ff2d4364f76c1a3f14f
David <dwhywood@gmail.com>
2018-10-22 14:48:47 -0500

    Create preorder method

--- Tree/SibTree.js
+++ Tree/SibTree.js
@@ -6,6 +6,18 @@ class SibTreeNode {
         this.nextSibling = nextSibling;
     }
 
+    preorder(visit) {
+        visit(this); 
+        if (this.firstChild !== null) {
+            this.firstChild.preorder(visit); 
+        }
+
+        if (this.nextSibling !== null) {
+            this.nextSibling.preorder(visit); 
+        }
+    }
+
+    // the length of the path from n to root 
     depth() {
         if (this.isRoot()) {
             return 0;
@@ -14,6 +26,7 @@ class SibTreeNode {
         }
     }
 
+    // the length of the path from n to its deepest descendant
     height() {
         var h = 0;
         var c = this.firstChild;
@@ -35,6 +48,17 @@ class SibTree {
     constructor(root) {
         this.root = root;
         this.size = 0;
+    }
+}
+
+function preorder(n, fn) {
+    fn(n); 
+    if (n.firstChild !== null) {
+        preorder(n.firstChild, fn); 
+    }
+
+    if (n.nextSibling !== null) {
+        preorder(n.nextSibling, fn); 
     }
 }
 
@@ -67,3 +91,5 @@ lec.parent = root;
 
 // console.log(height(lec));
 console.log(root.height());
+
+root.preorder((n) => { console.log(n.item) });
